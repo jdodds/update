6fdf4eee23eb31938a5b06de537b34e96698fc7f
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2012-01-16 13:37:57 -0500

    consolidate update lines

--- update.sh
+++ update.sh
@@ -11,32 +11,26 @@ function error
 
 function update_emacs
 {
-    info emacs updating
     cd $HOME/workspace/emacs
     bzr pull
-    (make && sudo make install && info emacs done) || (make bootstrap && make && sudo make install && info emacs done) || error emacs
+    (make && sudo make install) || (make bootstrap && make && sudo make install) || error emacs
 }
 
 function update_system
 {
-    info system updating
-    sudo pacman -Syu --noconfirm --logfile $HOME/log/update_system.log && info system updated || error "system update"
+    sudo pacman -Syu --noconfirm --logfile $HOME/log/update_system.log || error "system update"
 }
 
 function update_pear
 {
-    info pear updating
     sudo /usr/bin/pear clear-cache
     sudo /usr/bin/pear update-channels 2> $HOME/log/pear.log
     sudo /usr/bin/pear upgrade 2> $HOME/log/pear.log
-    info pear done
 }
 
 function update_org_feeds
 {
-    info org_feeds updating
     emacs --batch --eval '(load "~/.emacs")' -f 'org-feed-update-all'
-    info org_feeds done
 }
 
 function find_functions
@@ -47,7 +41,8 @@ function find_functions
 function update_all
 {
     for f in $(find_functions) ; do
-	[[ $f != "update_all" ]] && $f;
+	info update "running $f"
+	[[ $f != "update_all" ]] && $f && info update "$f done";
     done
 }
 
