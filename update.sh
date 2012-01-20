5b59dde9217e27dbbe68114fa742df89de2563ae
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2012-01-20 18:40:57 -0500

    add moodbar update function

--- update.sh
+++ update.sh
@@ -33,6 +33,38 @@ function update_org_feeds
     emacs --batch --eval '(load "~/.emacs")' -f 'org-feed-update-all'
 }
 
+function update_moodbar
+{
+    # from http://amarok.kde.org/wiki/Moodbar
+    DIR=/media/shared/Music/Music/
+    LAST=$HOME/.moodbar-lastreadsong
+    C_RET=0
+
+    control_c()        # run if user hits control-c
+    {
+	echo "$1" > "$LAST"
+	echo "Exiting..."
+	exit
+    }
+
+    if [ -e "$LAST" ]; then
+	read filetodelete < "$LAST"
+	rm "$filetodelete" "$LAST"
+    fi
+    exec 9< <(find "$DIR" -type f -regextype posix-awk -iregex '.*\.(mp3|ogg|flac|wma|m4a|mp4)') # you may need to add m4a and mp4
+    while read i
+    do
+	TEMP="${i%.*}.mood"
+	OUTF=`echo "$TEMP" | sed 's#\(.*\)/\([^,]*\)#\1/.\2#'`
+	trap 'control_c "$OUTF"' INT
+	if [ ! -e "$OUTF" ] || [ "$i" -nt "$OUTF" ]; then
+	    moodbar -o "$OUTF" "$i" || { C_RET=1; echo "An error occurred!" >&2; }
+	fi
+    done <&9
+    exec 9<&-
+}
+
+
 function find_functions
 {
     functions | grep -Eo '^(update_[_a-zA-Z]+)'
