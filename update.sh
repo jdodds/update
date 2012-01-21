c0102907bc14c203198f6e26f44fea0a7985c5be
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2012-01-21 12:18:39 -0500

    pull out actual functions into home configuration file

--- update.sh
+++ update.sh
@@ -1,73 +1,13 @@
 #!/usr/bin/env zsh
-function info
-{
-    notify-send -i info "$1" "$2"
-}
 
-function error
-{
-    notify-send -i error "$1" failed!
-}
+conf_dir=$HOME/.update
+funcs_dir=$HOME/.update/funcs
+conf_file=$HOME/.update/updaterc
 
-function update_emacs
-{
-    cd $HOME/workspace/emacs
-    bzr pull
-    (make && sudo make install) || (make bootstrap && make && sudo make install) || error emacs
-}
-
-function update_system
-{
-    sudo pacman -Syu --noconfirm --logfile $HOME/log/update_system.log || error "system update"
-}
-
-function update_pear
-{
-    sudo /usr/bin/pear clear-cache
-    sudo /usr/bin/pear update-channels 2> $HOME/log/pear.log
-    sudo /usr/bin/pear upgrade 2> $HOME/log/pear.log
-}
-
-function update_org_feeds
-{
-    emacs --batch --eval '(load "~/.emacs")' -f 'org-feed-update-all'
-}
-
-function update_moodbar
-{
-    # from http://amarok.kde.org/wiki/Moodbar
-    DIR=/media/shared/Music/Music/
-    LAST=$HOME/.moodbar-lastreadsong
-    C_RET=0
-
-    control_c()        # run if user hits control-c
-    {
-	echo "$1" > "$LAST"
-	echo "Exiting..."
-	exit
-    }
-
-    if [ -e "$LAST" ]; then
-	read filetodelete < "$LAST"
-	rm "$filetodelete" "$LAST"
-    fi
-    exec 9< <(find "$DIR" -type f -regextype posix-awk -iregex '.*\.(mp3|ogg|flac|wma|m4a|mp4)') # you may need to add m4a and mp4
-    while read i
-    do
-	TEMP="${i%.*}.mood"
-	OUTF=`echo "$TEMP" | sed 's#\(.*\)/\([^,]*\)#\1/.\2#'`
-	trap 'control_c "$OUTF"' INT
-	if [ ! -e "$OUTF" ] || [ "$i" -nt "$OUTF" ]; then
-	    moodbar -o "$OUTF" "$i" || { C_RET=1; echo "An error occurred!" >&2; }
-	fi
-    done <&9
-    exec 9<&-
-}
-
-function update_amarok_tags
-{
-    amarok_afttagger -r /media/shared/Music/Music/
-}
+. $conf_file
+for f in $(find $funcs_dir -type f); do
+    . $f;
+done
 
 function find_functions
 {
