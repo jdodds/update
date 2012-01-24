9f0d5571bfe1485d083beb31bed9fcfc2abbba1a
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2012-01-23 22:29:15 -0500

    add ability to blacklist functions from running in 'all' target by prefixing their name with 'no_auto_'

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 conf_dir=$HOME/.update
 funcs_dir=$HOME/.update/funcs
 conf_file=$HOME/.update/updaterc
+blacklist_token="no_auto_"
 
 . $conf_file
 for f in $(find $funcs_dir -type f); do
@@ -11,12 +12,17 @@ done
 
 function find_functions
 {
-    functions | grep -Eo '^(update_[_a-zA-Z]+)'
+    functions | grep -Eo '^('$blacklist_token')?(update_[_a-zA-Z]+)'
+}
+
+function find_auto_update_functions
+{
+    find_functions | grep -Ev '^('$blacklist_token')(update_[_a-zA-Z]+)'
 }
 
 function update_all
 {
-    for f in $(find_functions) ; do
+    for f in $(find_auto_update_functions) ; do
 	info update "running $f"
 	[[ $f != "update_all" ]] && $f && info update "$f done";
     done
@@ -24,13 +30,22 @@ function update_all
 
 function invalid_function
 {
-    funcs=$(find_functions | sed -e 's/update_/  /')
     echo "No update function for $1\n"
+    usage
+}
+
+function usage
+{
+    funcs=$(find_functions | sed -e 's/.*update_/  /')
     echo "Valid functions are:"
     echo "$funcs"
 }
 
 target="update_$1"
-[[ $(whence -w $target) != "$target: none" ]] && \
-    $target || \
-    invalid_function $1
+found=$(whence -w $target)
+if [[ $found == "$target: none" ]] ; then
+    target="$blacklist_token$target"
+    found=$(whence -w $target)
+fi
+
+[[ $found != "$target: none" ]] && $target || invalid_function $1
