de5f75dd2002d5447c945865d47a6d103b1c2c09
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2012-02-02 15:11:54 -0500

    pull out running of update into running of action, and add the file and template for 'check' action

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env zsh
 
-conf_dir=$HOME/.update
-funcs_dir=$HOME/.update/funcs
-conf_file=$HOME/.update/updaterc
+action=$1
+conf_dir=$HOME/.$action
+funcs_dir=$HOME/.$action/funcs
+conf_file=$HOME/.$action/"$action"rc
 blacklist_token="no_auto_"
+
 
 . $conf_file
 for f in $(find $funcs_dir -type f); do
@@ -12,40 +14,41 @@ done
 
 function find_functions
 {
-    functions | grep -Eo '^('$blacklist_token')?(update_[_a-zA-Z]+)'
+    functions | grep -Eo '^('$blacklist_token')?('$action'_[_a-zA-Z]+)'
 }
 
-function find_auto_update_functions
+function find_auto_functions
 {
-    find_functions | grep -Ev '^('$blacklist_token')(update_[_a-zA-Z]+)'
+    find_functions | grep -Ev '^('$blacklist_token')('$action'_[_a-zA-Z]+)'
 }
 
-function update_all
+function all
 {
-    for f in $(find_auto_update_functions) ; do
-	info update "running $f"
-	[[ $f != "update_all" ]] && $f && info update "$f done" || error "$f";
+    for f in $(find_auto_functions) ; do
+	info $action "running $f"
+	[[ $f != "$action_all" ]] && $f && info $action "$f done" || error "$f";
     done
 }
 
 function invalid_function
 {
-    echo "No update function for $1\n"
+    echo "No $action function for $1\n"
     usage
 }
 
 function usage
 {
-    funcs=$(find_functions | sed -e 's/.*update_/  /')
+    funcs=$(find_functions | sed -e 's/.*'$action'_/  /')
     echo "Valid functions are:"
     echo "$funcs"
 }
 
-target="update_$1"
+target="$action"_"$2"
 found=$(whence -w $target)
 if [[ $found == "$target: none" ]] ; then
     target="$blacklist_token$target"
     found=$(whence -w $target)
 fi
 
-[[ $found != "$target: none" ]] && $target || invalid_function $1
+[[ $found != "$target: none" ]] && $target || invalid_function $2
+
